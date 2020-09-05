1bf290769c1ceb14aa7f1033303361e1346b30c5
Kyle Parrish <kfparri@gmail.com>
2020-09-05 08:40:56 -0500

    Initial commit

---
+++ src/DataAccessParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Natadon.DataAccess.Core
+{
+    public class DataAccessParameter
+    {
+        public string ParameterName { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ src/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Natadon.DataAccess.Core
+{
+    public interface IDataAccess
+    {
+        void ExecuteQuery(string SqlStatement, List<DataAccessParameter> Params = null);
+
+        DataSet GetDataSet(string SqlStatement, List<DataAccessParameter> Params = null);
+
+        object GetScalar(string SqlStatement, List<DataAccessParameter> Params = null);
+    }
+}
